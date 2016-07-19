d4d660eda139366c8dff554c46a4c4391a1e8567
Mark Veenstra <mark@mobilea.nl>
2016-07-19 14:37:54 +0200

    Revert "Merge with 3.1.0"

--- src/PluginRTCPeerConnection.swift
+++ src/PluginRTCPeerConnection.swift
@@ -8,8 +8,6 @@ class PluginRTCPeerConnection : NSObject, RTCPeerConnectionDelegate, RTCSessionD
 	var pluginRTCPeerConnectionConstraints: PluginRTCPeerConnectionConstraints
 	// PluginRTCDataChannel dictionary.
 	var pluginRTCDataChannels: [Int : PluginRTCDataChannel] = [:]
-	// PluginRTCDTMFSender dictionary.
-	var pluginRTCDTMFSenders: [Int : PluginRTCDTMFSender] = [:]
 	var eventListener: (data: NSDictionary) -> Void
 	var eventListenerForAddStream: (pluginMediaStream: PluginMediaStream) -> Void
 	var eventListenerForRemoveStream: (id: String) -> Void
@@ -40,7 +38,6 @@ class PluginRTCPeerConnection : NSObject, RTCPeerConnectionDelegate, RTCSessionD
 
 	deinit {
 		NSLog("PluginRTCPeerConnection#deinit()")
-		self.pluginRTCDTMFSenders = [:]
 	}
 
 
@@ -316,31 +313,6 @@ class PluginRTCPeerConnection : NSObject, RTCPeerConnectionDelegate, RTCSessionD
 	}
 
 
-	func createDTMFSender(
-		dsId: Int,
-		track: PluginMediaStreamTrack,
-		eventListener: (data: NSDictionary) -> Void
-	) {
-		NSLog("PluginRTCPeerConnection#createDTMFSender()")
-
-		if self.rtcPeerConnection.signalingState.rawValue == RTCSignalingClosed.rawValue {
-			return
-		}
-
-		let pluginRTCDTMFSender = PluginRTCDTMFSender(
-			rtcPeerConnection: rtcPeerConnection,
-			track: track.rtcMediaStreamTrack,
-			eventListener: eventListener
-		)
-
-		// Store the pluginRTCDTMFSender into the dictionary.
-		self.pluginRTCDTMFSenders[dsId] = pluginRTCDTMFSender
-
-		// Run it.
-		pluginRTCDTMFSender.run()
-	}
-
-
 	func close() {
 		NSLog("PluginRTCPeerConnection#close()")
 
@@ -411,27 +383,6 @@ class PluginRTCPeerConnection : NSObject, RTCPeerConnectionDelegate, RTCSessionD
 
 		// Remove the pluginRTCDataChannel from the dictionary.
 		self.pluginRTCDataChannels[dcId] = nil
-	}
-
-
-	func RTCDTMFSender_insertDTMF(
-		dsId: Int,
-		tones: String,
-		duration: Int,
-		interToneGap: Int
-	) {
-		NSLog("PluginRTCPeerConnection#RTCDTMFSender_insertDTMF()")
-
-		if self.rtcPeerConnection.signalingState.rawValue == RTCSignalingClosed.rawValue {
-			return
-		}
-
-		let pluginRTCDTMFSender = self.pluginRTCDTMFSenders[dsId]
-		if pluginRTCDTMFSender == nil {
-			return
-		}
-
-		pluginRTCDTMFSender!.insertDTMF(tones, duration: duration, interToneGap: interToneGap)
 	}
 
 
